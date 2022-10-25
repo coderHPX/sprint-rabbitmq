dfb2707b84e1d60987efe8e2e915926a9a4d54f0
hpx <2976633013@qq.com>
2022-10-25 18:17:33 +0800

    Test.java commit

---
+++ src/main/java/com/rabbitmq/hpx/test/Test.java
@@ -0,0 +1,14 @@
+package com.rabbitmq.hpx.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hpx");
+        System.out.println("hpx");
+        System.out.println("hpx");
+        System.out.println("hpx");
+        System.out.println("hpx");
+        System.out.println("hpx");
+        System.out.println("hpx");
+        System.out.println("master");
+    }
+}
