cd70c17446cf714fb426aee252c89b3c3c790e53
hpx <2976633013@qq.com>
2022-10-25 18:21:09 +0800

    master commit

--- src/main/java/com/rabbitmq/hpx/test/Test.java
+++ src/main/java/com/rabbitmq/hpx/test/Test.java
@@ -13,6 +13,7 @@ public class Test {
         System.out.println("master++++++");
         System.out.println("hpx");
         System.out.println("hot");
+        System.out.println("master");
 
     }
 }
